b26cee6fdd73dc04f0c1ff9c2c390cd45b0d977f
Sohel Amin <sohelamincse@gmail.com>
2015-07-09 17:25:47 +0600

    configuration changes

--- lamp.sh
+++ lamp.sh
@@ -1,14 +1,19 @@
+#!/bin/bash
+# ********************************
+# Project: LAMP Setup Script
+# Author: Sohel Amin
+# URL: http://www.appzcoder.com
+# ********************************
+
 # Variables
-APPENV=local
-DBHOST=localhost
 DBNAME=devdb
 DBUSER=root
 DBPASSWD=1234
 
 sudo apt-get update
 
-sudo apt-get install -y vim-nox curl python-software-properties build-essential git
-sudo add-apt-repository -y ppa:ondrej/php5
+# Installing curl, git apache2
+sudo apt-get install -y curl git apache2
 
 sudo apt-get update
 
@@ -27,7 +32,7 @@ mysql -uroot -p$DBPASSWD -e "CREATE DATABASE $DBNAME"
 mysql -uroot -p$DBPASSWD -e "grant all privileges on $DBNAME.* to '$DBUSER'@'localhost' identified by '$DBPASSWD'"
  
 # Installing php5 with mcrypt and other packages
-sudo apt-get install -y php5 apache2 libapache2-mod-php5 php5-curl php5-gd php5-mcrypt php5-readline mysql-server-5.5 php5-mysql git-core php5-xdebug
+sudo apt-get install -y php5 libapache2-mod-php5 php5-curl php5-gd php5-mcrypt php5-readline mysql-server-5.5 php5-mysql git-core php5-xdebug
 
 cat << EOF | sudo tee -a /etc/php5/mods-available/xdebug.ini
 xdebug.scream=1
@@ -45,6 +50,10 @@ sudo a2enmod rewrite
 sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php5/apache2/php.ini
 sed -i "s/display_errors = .*/display_errors = On/" /etc/php5/apache2/php.ini
 sed -i "s/disable_functions = .*/disable_functions = /" /etc/php5/cli/php.ini
+
+# Making info.php file for display phpinfo
+sudo chmod 777 -R /var/www/;
+sudo printf "<?php\nphpinfo();\n?>" > /var/www/html/info.php;
 
 # Restarting apache2
 sudo service apache2 restart
