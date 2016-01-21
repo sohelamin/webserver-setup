0dcc0b81aecdf10f08d99714381cd7aa70b4fc4c
Sohel Amin <sohelamincse@gmail.com>
2016-01-21 20:48:31 +0600

    cli error reporting turn off

--- lamp.sh
+++ lamp.sh
@@ -63,7 +63,6 @@ sudo a2enmod rewrite
 sudo sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php5/apache2/php.ini
 sudo sed -i "s/display_errors = .*/display_errors = On/" /etc/php5/apache2/php.ini
 sudo sed -i "s/display_startup_errors = .*/display_startup_errors = On/" /etc/php5/apache2/php.ini
-sudo sed -i "s/disable_functions = .*/disable_functions = /" /etc/php5/cli/php.ini
 
 # Making info.php file for display phpinfo
 sudo chmod 777 -R /var/www/;
